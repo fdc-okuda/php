252508840cd9257225dd1bbc8d87e89d7404f12a
Yutaro Okuda <okuda.fdc@gmail.com>
2022-09-02 10:36:04 +0800

    main: count modified

--- cart/index.php
+++ cart/index.php
@@ -15,13 +15,26 @@ if(isset($_POST["action_add_to_cart"]) && !empty($_POST["action_add_to_cart"])){
 
     $arrShoppingCart = $_SESSION["shopping_cart"];
 
-    $arrShoppingCart[] = [
-        "form_image" => $form_image,
-        "form_name" => $form_name,
-        "form_code" => $form_code,
-        "form_quantity" => $form_quantity,
-        "form_price" => $form_price,
-    ];
+    $arr_found_id = false;
+    
+    for($i=0; $i < count($arrShoppingCart); $i++){
+        $item = $arrShoppingCart[$i];
+        if($form_code == $item["form_code"]){
+            $arr_found_id = $i;
+        }
+    }
+    if($arr_found_id !== false){
+        $arrShoppingCart[$arr_found_id]['form_quantity'] = $arrShoppingCart[$arr_found_id]['form_quantity']+$form_quantity;
+        
+    } else {
+        $arrShoppingCart[] = [
+            "form_image" => $form_image,
+            "form_name" => $form_name,
+            "form_code" => $form_code,
+            "form_quantity" => $form_quantity,
+            "form_price" => $form_price,
+        ];
+    }
 
     $_SESSION["shopping_cart"] = array_values($arrShoppingCart);
 
