9d31ba5d4b1af0583968ba15502794e32d24ef33
Yutaro Okuda <okuda.fdc@gmail.com>
2022-09-08 16:25:46 +0800

    main: modified

--- cal/calendar.php
+++ cal/calendar.php
@@ -1,44 +1,73 @@
+<?php date_default_timezone_set ('Asia/Manila'); ?>
+
 <?php 
 session_start();
+$is_edit = false;
 
+//submitを押されたら
 if(isset($_POST["add_calendar"]) && !empty($_POST["add_calendar"])){
+   
+   //入力された内容を変数にいれる
    $form_title = $_POST['form_title'];
    $form_date = $_POST['form_date'];
    $form_textarea = $_POST['form_textarea'];
+   $form_image = $_POST['file'];
    
    // only process if has valid date
    if(!empty($form_date)){
+    
+        $file = $_FILES['file'];
+
+        $fileName = $_FILES['file']['name'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileError = $_FILES['file']['error'];
+        $fileType = $_FILES['file']['type'];
+
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+
+        $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+        if (in_array($fileActualExt, $allowed)) {
+            if ($fileError === 0){
+                if($fileSize < 500000000) {
+                    $fileNameNew = uniqid('', true).".".$fileActualExt;
+                    $fileDestination = 'uploads/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                } else {
+                    echo "Your file is too big!";
+                    header('Location: ./');
+                    exit;
+                }
+            } else {
+            echo "There was an error uploading your file!";
+            header('Location: ./');
+            exit;
+            }
+        } else {
+            echo "You cannot upload files of this type!";
+            header('Location: ./');
+            exit;
+        }
+
         // if there is no session date yet
         if(!isset($_SESSION[$form_date])){
             $_SESSION[$form_date] = [];
         }
 
+        // セッションに情報を入れる
         $_SESSION[$form_date][] = [
             "title" => $form_title,
             "body" => $form_textarea,
             "date" => $form_date,
-            "status" => 0,
+            "image" => $fileNameNew,
         ];
-   }
+   
+        header('Location: ./');
+        exit;
 
-   if($_SESSION[$form_date] == '2022-09-07'){
-        $_SESSION[$form_date]= [
-            "status" == 7,
-        ];
-   }
-
-   if($_SESSION[$form_date] == '2022-09-10'){
-    $_SESSION[$form_date]= [
-        "status" == 10,
-    ];
-}
-
-   if($_SESSION[$form_date] == '2022-09-30'){
-    $_SESSION[$form_date]= [
-        "status" == 30,
-    ];
     }
-
 }
 
 function displayEventDates ($date) {
@@ -54,17 +83,115 @@ function displayEventDates ($date) {
 
     for ($i = 0; $i < count($_SESSION[$date]); $i++) {
         $event = $_SESSION[$date][$i];
-?>
-        <form action="">
-            <div>   
-                <?php echo $event["title"]; ?>
-            </div>
-        </form>
+?>  
+        <div class="day">
+            <img src="uploads/<?php echo $event["image"];?>" alt="" class="day_image">
+            <div class="day_title"><?php echo $event["title"]; ?></div>
+        </div>
+        <div class="today_right">   
+            <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" class="edit_button">
+                <input type="submit" name="form_edit" value="edit" style="height: 15px; margin-top: 1px; padding-top: 0px; font-size: 80%;">
+                <input type="hidden" name="form_edit_id" value="<?php echo $i; ?>">
+                <input type="hidden" name="form_edit_date" value="<?php echo $date; ?>">
+                <input type="hidden" name="form_edit_image " value="<?php echo $image; ?>">
+            </form>
+
+
+            <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" class="delete_btn">
+                <input type="submit" name="form_delete" value="delete"  style="height: 15px; padding-top: 0px; font-size: 80%;">
+                <input type="hidden" name="form_delete_id" value="<?php echo $i; ?>">
+                <input type="hidden" name="form_delete_date" value="<?php echo $date; ?>">
+                <input type="hidden" name="form_edit_date" value="<?php echo $image; ?>">
+            </form>
+        </div>
 <?php
     }
 }
 
+function reminder ($date) {
+    if(isset($_SESSION[$date])){
+        //同じ日付にある予定の数だけforをくりかえす。
+    for ($i = 0; $i < count($_SESSION[$date]); $i++) {
+        $event = $_SESSION[$date][$i];
+?>
+        <form action="<?php $_SERVER['PHP_SELF']; ?>" class="today">
+            <div class="today_left">   
+                <!-- その日のうちの一つの予定のタイトルを取得し、表示する。 -->
+                "<?php echo $event["title"]; ?>"
+                <span class="top_body"><?php echo $event["body"]; ?></span>
+            </div>
+            
+        </form>
+<?php
+    }
+}
+}
+
 // echo "<pre>";
 // var_dump($_SESSION);
+
 ?>
 
+<!-- deleteボタンが押されたら -->
+<?php
+if(isset($_POST["form_delete"]) && !empty($_POST["form_delete"])){
+    $index = $_POST["form_delete_id"];
+    $date = $_POST["form_delete_date"];
+    $cal = $_SESSION;
+    
+    unset($cal[$date][$index]);
+
+    $_SESSION = ($cal);
+};
+?>
+
+<!-- editボタンが押されたら -->
+<?php
+if(isset($_POST["form_edit"]) && !empty($_POST["form_edit"])){
+    $index = $_POST["form_edit_id"];
+    $date = $_POST["form_edit_date"];
+    $is_edit = true;
+    $cal = $_SESSION;
+    $event = $cal[$date][$index];
+    
+    $edit_title = $event["title"];
+    $edit_body = $event["body"];
+    $edit_date = $event["date"];
+    $edit_image = $event["image"];
+    
+}
+?>
+
+<!-- 編集後にsubmitボタンが押されたら -->
+<?php
+if(isset($_POST["edit_calendar"]) && !empty($_POST["edit_calendar"])){
+    $edited_title = $_POST["form_title"];
+    $edited_date = $_POST["form_date"];
+    $edited_textarea = $_POST["form_textarea"];
+    $edited_index = $_POST["edit_calendar_index"];
+    $edited_calendar_date = $_POST["edit_calendar_date"];
+    
+    
+    $cal = $_SESSION;
+    
+    if(isset($cal[$edited_calendar_date][$edited_index])){
+        if($edited_calendar_date != $edited_date){
+            $cal[$edited_date][] = [
+                "title" => $edited_title,
+                "body" => $edited_textarea,
+                "date" => $edited_date,
+              
+            ];
+            unset($cal[$edited_calendar_date][$edited_index]);
+        } else {
+            $cal[$edited_calendar_date][$edited_index] = [
+                "title" => $edited_title,
+                "body" => $edited_textarea,
+                "date" => $edited_date,
+                
+            ];
+        }
+        $_SESSION = ($cal);
+    }
+}
+?>
--- cal/index.php
+++ cal/index.php
@@ -14,47 +14,146 @@
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&display=swap" rel="stylesheet">
     <style>
         .container{
+            margin-top: 20px;
             font-family: 'M PLUS Rounded 1c', sans-serif;
+        }
+        .header{
+            margin-top: 30px;
+            margin-bottom: 15px;
         }
         .table{
             table-layout: fixed;
+        }
+        .t_reminder{
+            height: 50px;
         }
         .input-above{
             width: 100%;
             display: flex;
             justify-content: space-between;
         }
+        .input-below{
+            /* position: relative; */
+            margin-bottom: 50px;
+        }
+        .button{
+            /* position: absolute;
+            bottom: 0;
+            right: 0; */
+        }
         .title{
             width: 65%;
+            border: solid 1.5px #DEE2E6;
         }
         .date{
             width: 35%;
-        }
-        .input-below{
-            
+            border: solid 1.5px #DEE2E6;
+            color: #848484;
         }
         .textarea{
             width: 100%;
             height: 100px;
+            font-family: 'M PLUS Rounded 1c', sans-serif;
+            border: solid 1.5px #DEE2E6;
+        }
+        .square{
+            border: solid 1.5px #DEE2E6;
+            width: 100%;
+            height: 100px;
+            margin-bottom: 50px;
         }
         td{
             height: 100px;
+        }
+        .cell{
+            position: relative;
+        }
+        .cell span{
+            position: absolute;
+            right: 10px;
+            bottom: 5px;
+        }
+        .day{
+            display: flex;
+            justify-content: flex-start;
+        }
+        .day_image{
+            width: 10px;
+            height: 10px;
+            margin-top: 4px;
+        }
+        .day_title{
+            margin-left: 5px;
+        }
+        .today{
+            display: flex;
+            margin-bottom: 10px;
+        }
+        .today_right{
+            display: flex;
+            margin-bottom: 5px;
+        }
+        .top_body{
+            margin-left: 10px;
+        }
+        .delete_btn{
+            margin-left: 5px;
+            margin-top: 0px;
         }
     </style>
     <title>calender</title>
 </head>
 <body>
     <div class="container">
-        <form action="<?php $_SERVER['PHP_SELF']; ?>" class="submit-button" method="POST" >
+
+        <h1>September</h1>
+
+        <h3 class="header">Today's Schedule</h3>
+
+        <table class="table">
+            <tr>
+                <td class="t_reminder">
+                    <?php $timestamp = time();
+                        $day = date("Y-m-d", $timestamp);
+                        reminder("$day");
+                    ?>
+                </td>
+            </tr>   
+        </table>
+
+        <h3 class="header">Keep Your Plan</h3>
+
+        <?php if ($is_edit == false){ ?>
+        <form action="<?php $_SERVER['PHP_SELF']; ?>" class="submit-button" method="POST" enctype="multipart/form-data" >
             <div class="input-above">
-                <input type="text" class="title" name="form_title">
-                <input type="date" class="date" name="form_date">
+                <input type="text" class="title" name="form_title" required="required" placeholder="title">
+                <input type="date" class="date" name="form_date" required="required">
             </div>
             <div class="input-below">
-                <input type="textarea" class="textarea" name="form_textarea">
-                <input type="submit" class="button" value="submit" type="submit" name="add_calendar" style="float:right">    
+                <input type="textarea" class="textarea" name="form_textarea" required="required" placeholder="content">
+                <input type="file" name="file" required="required">
+                <input type="submit" class="button" value="submit" type="submit" name="add_calendar" style="float: right;">   
             </div>
+        </form>
+
+    <?php } ?>
+
+    <?php if ($is_edit == true){ ?>
+       <form action="<?php $_SERVER['PHP_SELF']; ?>" class="submit-button" method="POST" enctype="multipart/form-data">
+            <div class="input-above">
+                <input type="text" class="title" name="form_title" required="required" value = "<?php echo $edit_title ?>" placeholder="title">
+                <input type="date" class="date" name="form_date" required="required" value = "<?php echo $edit_date ?>">
+            </div>
+            <div class="input-below">
+                <textarea class="textarea" name="form_textarea" required="required"><?php echo $edit_body ?></textarea>
+                <input type="submit" class="button" value="submit" type="submit" name="edit_calendar" style="float: right;">    
+                <input type="hidden" value="<?php echo $date ?>" name="edit_calendar_date">
+                <input type="hidden" value="<?php echo $index ?>" name="edit_calendar_index">
+                <input type="file" value="<?php echo $image ?>" name="edit_calendar_image"  required="required">
+            </div>
+            
        </form>
+       <?php } ?>
         
        <table class="table table-bordered">
                 <thead>
@@ -69,52 +168,49 @@
                     </tr>
                 </thead>
                 <tbody>
-                <?php 
-                    $arrCalendarItems = $_SESSION;
-                ?>
                     <tr>
                         <td></td>
                         <td></td>
                         <td></td>
                         <td></td>
-                        <td><?php displayEventDates("2022-09-01"); ?></td>
-                        <td><?php displayEventDates("2022-09-02"); ?></td>
-                        <td><?php displayEventDates("2022-09-03"); ?></td>
+                        <td class="cell"><span class="number">1</span><?php displayEventDates("2022-09-01"); ?></td>
+                        <td class="cell"><span class="number">2</span><?php displayEventDates("2022-09-02"); ?></td>
+                        <td class="cell"><span class="number">3</span><?php displayEventDates("2022-09-03"); ?></td>
                     </tr>
                     <tr>
-                        <td><?php displayEventDates("2022-09-04"); ?></td>
-                        <td><?php displayEventDates("2022-09-05"); ?></td>
-                        <td><?php displayEventDates("2022-09-06"); ?></td>
-                        <td><?php displayEventDates("2022-09-07"); ?></td>
-                        <td><?php displayEventDates("2022-09-08"); ?></td>
-                        <td><?php displayEventDates("2022-09-09"); ?></td>
-                        <td><?php displayEventDates("2022-09-10"); ?></td>
+                        <td class="cell"><span class="number">4</span><?php displayEventDates("2022-09-04"); ?></td>
+                        <td class="cell"><span class="number">5</span><?php displayEventDates("2022-09-05"); ?></td>
+                        <td class="cell"><span class="number">6</span><?php displayEventDates("2022-09-06"); ?></td>
+                        <td class="cell"><span class="number">7</span><?php displayEventDates("2022-09-07"); ?></td>
+                        <td class="cell"><span class="number">8</span><?php displayEventDates("2022-09-08"); ?></td>
+                        <td class="cell"><span class="number">9</span><?php displayEventDates("2022-09-09"); ?></td>
+                        <td class="cell"><span class="number">10</span><?php displayEventDates("2022-09-10"); ?></td>
                     </tr>
                     <tr>
-                        <td><?php displayEventDates("2022-09-11"); ?></td>
-                        <td><?php displayEventDates("2022-09-12"); ?></td>
-                        <td><?php displayEventDates("2022-09-13"); ?></td>
-                        <td><?php displayEventDates("2022-09-14"); ?></td>
-                        <td><?php displayEventDates("2022-09-15"); ?></td>
-                        <td><?php displayEventDates("2022-09-16"); ?></td>
-                        <td><?php displayEventDates("2022-09-17"); ?></td>
+                        <td class="cell"><span class="number">11</span><?php displayEventDates("2022-09-11"); ?></td>
+                        <td class="cell"><span class="number">12</span><?php displayEventDates("2022-09-12"); ?></td>
+                        <td class="cell"><span class="number">13</span><?php displayEventDates("2022-09-13"); ?></td>
+                        <td class="cell"><span class="number">14</span><?php displayEventDates("2022-09-14"); ?></td>
+                        <td class="cell"><span class="number">15</span><?php displayEventDates("2022-09-15"); ?></td>
+                        <td class="cell"><span class="number">16</span><?php displayEventDates("2022-09-16"); ?></td>
+                        <td class="cell"><span class="number">17</span><?php displayEventDates("2022-09-17"); ?></td>
                     </tr>
                     <tr>
-                        <td><?php displayEventDates("2022-09-18"); ?></td>
-                        <td><?php displayEventDates("2022-09-19"); ?></td>
-                        <td><?php displayEventDates("2022-09-20"); ?></td>
-                        <td><?php displayEventDates("2022-09-21"); ?></td>
-                        <td><?php displayEventDates("2022-09-22"); ?></td>
-                        <td><?php displayEventDates("2022-09-23"); ?></td>
-                        <td><?php displayEventDates("2022-09-24"); ?></td>
+                        <td class="cell"><span class="number">18</span><?php displayEventDates("2022-09-18"); ?></td>
+                        <td class="cell"><span class="number">19</span><?php displayEventDates("2022-09-19"); ?></td>
+                        <td class="cell"><span class="number">20</span><?php displayEventDates("2022-09-20"); ?></td>
+                        <td class="cell"><span class="number">21</span><?php displayEventDates("2022-09-21"); ?></td>
+                        <td class="cell"><span class="number">22</span><?php displayEventDates("2022-09-22"); ?></td>
+                        <td class="cell"><span class="number">23</span><?php displayEventDates("2022-09-23"); ?></td>
+                        <td class="cell"><span class="number">24</span><?php displayEventDates("2022-09-24"); ?></td>
                     </tr>
                     <tr>
-                        <td><?php displayEventDates("2022-09-25"); ?></td>
-                        <td><?php displayEventDates("2022-09-26"); ?></td>
-                        <td><?php displayEventDates("2022-09-27"); ?></td>
-                        <td><?php displayEventDates("2022-09-28"); ?></td>
-                        <td><?php displayEventDates("2022-09-29"); ?></td>
-                        <td><?php displayEventDates("2022-09-30"); ?></td>
+                        <td class="cell"><span class="number">25</span><?php displayEventDates("2022-09-25"); ?></td>
+                        <td class="cell"><span class="number">26</span><?php displayEventDates("2022-09-26"); ?></td>
+                        <td class="cell"><span class="number">27</span><?php displayEventDates("2022-09-27"); ?></td>
+                        <td class="cell"><span class="number">28</span><?php displayEventDates("2022-09-28"); ?></td>
+                        <td class="cell"><span class="number">29</span><?php displayEventDates("2022-09-29"); ?></td>
+                        <td class="cell"><span class="number">30</span><?php displayEventDates("2022-09-30"); ?></td>
                         <td></td>
                     </tr>
                 </tbody>
